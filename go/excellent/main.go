95f6c29ec6d506dd506f6b9bce9217b1fe13afb5
soblin <hilo.soblin@gmail.com>
2024-07-23 12:25:05 +0900

    change main.go
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- go/excellent/main.go
+++ go/excellent/main.go
@@ -1,7 +1,10 @@
 package main
 
 func EvenOrOdd(number int) string {
-	if number % 2 == 0 {
+	if number % 2 == 10 {
+		return "even"
+	}
+	else if number % 2 == 0 {
 		return "even"
 	} else {
 		return "odd"
